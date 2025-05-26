49b26b8a094e0367793801e4cf01c4f55804a6cc
tonho <joaoamlp@gmail.com>
2025-05-26 10:26:16 -0300

    modified algorithms to get data

--- checkSumAlgorithms/bruteForceCheckSum.py
+++ checkSumAlgorithms/bruteForceCheckSum.py
@@ -1,7 +1,8 @@
 import time
 import os
 import csv
-
+import random
+ 
 def get_elapsed_us(start, end):
     return (end - start) * 1_000_000
 
@@ -35,25 +36,18 @@ def subset_sum_dp(arr, target, filename):
         start_element = time.perf_counter()
         
         idx = prev[curr] - 1
-        print(f"\n\t{arr[idx]} ", end="")
         curr -= arr[idx]
         
         end_element = time.perf_counter()
         elapsed_us = get_elapsed_us(start_element, end_element)
-        print(f"(tempo para este elemento: {elapsed_us:.3f} microsegundos)", end="")
-    
-    print("\n}")
-    
-    print(f"Tempo total da busca: {total_time_ms:.2f} milissegundos")
-    print(f"ou {total_time_ms / 1000:.4f} segundos")
-    
+       
     # Salva os resultados no CSV
     save_to_csv(filename, target, total_time_ms, True)
     
     return True
 
 def save_to_csv(filename, target, total_time_ms, found):
-    csv_filename = os.path.join("..", "data", "subset_results.csv")
+    csv_filename = os.path.join("..", "data", "dataBasePython.csv")
     
     # Verifica se o arquivo já existe para decidir se escreve o cabeçalho
     file_exists = os.path.isfile(csv_filename)
@@ -62,24 +56,27 @@ def save_to_csv(filename, target, total_time_ms, found):
         writer = csv.writer(file)
         
         if not file_exists:
-            writer.writerow(["Arquivo", "Target", "Tempo (ms)", "Encontrado"])
+            writer.writerow(["Target", "Tempo total (ms)","Arquivo" ])
         
-        writer.writerow([filename, target, f"{total_time_ms:.2f}", "Sim" if found else "Não"])
+        writer.writerow([ target, f"{total_time_ms:.2f}", filename])
 
 def main():
     filename = input("Digite o nome do arquivo (ex: subset_1000.txt): ")
-    target = int(input("Digite o valor da soma alvo: "))
-    
-    fullpath = os.path.join("..", "data", filename)
-    
-    try:
-        with open(fullpath, "r") as file:
-            arr = [int(line.strip()) for line in file if line.strip()]
-    except FileNotFoundError:
-        print("Erro ao abrir o arquivo.")
-        return
-    
-    subset_sum_dp(arr, target, filename)
+    print("Iniciando a insercao")
+
+    for i in range(100):
+        target = random.randrange(1,100000 )
+        
+        fullpath = os.path.join("..", "data", filename)
+        
+        try:
+            with open(fullpath, "r") as file:
+                arr = [int(line.strip()) for line in file if line.strip()]
+        except FileNotFoundError:
+            print("Erro ao abrir o arquivo.")
+            return
+        
+        subset_sum_dp(arr, target, filename)
 
 if __name__ == "__main__":
     main()
